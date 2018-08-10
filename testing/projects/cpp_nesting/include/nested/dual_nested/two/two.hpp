0fac74b47e12e7936a2dcc7898234cbfe4fabe2b
sven <sjm324@cornell.edu>
2018-08-10 00:51:58 -0700

    initial CMake build system for testing/projects
    - adds tests from cpp_nesting project
    - setup for code coverage on travis ready
    - AppVeyor // OpenCppCoverage will need some CMake tweaks

--- testing/projects/cpp_nesting/include/nested/dual_nested/two/two.hpp
+++ testing/projects/cpp_nesting/include/nested/dual_nested/two/two.hpp
@@ -1,8 +1,17 @@
+/***************************************************************************************
+ * This file is dedicated to the public domain.  If your jurisdiction requires a       *
+ * specific license:                                                                   *
+ *                                                                                     *
+ * Copyright (c) Stephen McDowell, 2017-2018                                           *
+ * License:      CC0 1.0 Universal                                                     *
+ * License Text: https://creativecommons.org/publicdomain/zero/1.0/legalcode           *
+ **************************************************************************************/
 /**
  * \file
  *
  * \brief This file should be two levels down in the hierarchy.
  */
+#pragma once
 
 namespace nested {
     namespace dual_nested {
@@ -16,7 +25,13 @@ namespace nested {
                     std::uint16_t s[2];///< occupies 4 bytes
                     std::uint8_t c;    ///< occupies 1 byte
                 };
+
+                /// Returns ``true`` ;)
+                bool compiles() { return true; }
             };
+
+            /// Returns ``true`` ;)
+            bool compiles() { return true; }
         };
     }
 }
--- testing/projects/cpp_nesting/include/nested/two/two.hpp
+++ testing/projects/cpp_nesting/include/nested/two/two.hpp
@@ -1,8 +1,17 @@
+/***************************************************************************************
+ * This file is dedicated to the public domain.  If your jurisdiction requires a       *
+ * specific license:                                                                   *
+ *                                                                                     *
+ * Copyright (c) Stephen McDowell, 2017-2018                                           *
+ * License:      CC0 1.0 Universal                                                     *
+ * License Text: https://creativecommons.org/publicdomain/zero/1.0/legalcode           *
+ **************************************************************************************/
 /**
  * \file
  *
  * \brief This file should be one level down in the hierarchy.
  */
+#pragma once
 
 namespace nested {
     /// The second class inside the ``nested`` namespace.
@@ -15,7 +24,13 @@ namespace nested {
                 std::uint16_t s[2];///< occupies 4 bytes
                 std::uint8_t c;    ///< occupies 1 byte
             };
+
+            /// Returns ``true`` ;)
+            bool compiles() { return true; }
         };
+
+        /// Returns ``true`` ;)
+        bool compiles() { return true; }
     };
 
     /// A union that occupies four bytes: http://en.cppreference.com/w/cpp/language/union
