019b89b3c24971057b60db6508d619baa2a632bb
sven <sjm324@cornell.edu>
2018-08-10 00:51:58 -0700

    initial CMake build system for testing/projects
    - adds tests from cpp_nesting project
    - setup for code coverage on travis ready
    - AppVeyor // OpenCppCoverage will need some CMake tweaks

--- testing/projects/cpp_nesting/include/nested/one/one.hpp
+++ testing/projects/cpp_nesting/include/nested/one/one.hpp
@@ -1,8 +1,17 @@
+/***************************************************************************************
+ * This file is dedicated to the public domain.  If your jurisdiction requires a       *
+ * specific license:                                                                   *
+ *                                                                                     *
+ * Copyright (c) Stephen McDowell, 2017-2018                                           *
+ * License:      CC0 1.0 Universal                                                     *
+ * License Text: https://creativecommons.org/publicdomain/zero/1.0/legalcode           *
+ **************************************************************************************/
 /**
  * \file
  *
  * \brief This file should be one level down in the hierarchy.
  */
+#pragma once
 
 namespace nested {
     /// The first class inside the ``nested`` namespace.
@@ -15,6 +24,12 @@ namespace nested {
                 std::uint16_t s[2];///< occupies 4 bytes
                 std::uint8_t c;    ///< occupies 1 byte
             };
+
+            /// Returns ``true`` ;)
+            bool compiles() { return true; }
         };
+
+        /// Returns ``true`` ;)
+        bool compiles() { return true; }
     };
 }
---
+++ testing/projects/cpp_nesting/src/tests.cpp
@@ -0,0 +1,70 @@
+/***************************************************************************************
+ * This file is dedicated to the public domain.  If your jurisdiction requires a       *
+ * specific license:                                                                   *
+ *                                                                                     *
+ * Copyright (c) Stephen McDowell, 2017-2018                                           *
+ * License:      CC0 1.0 Universal                                                     *
+ * License Text: https://creativecommons.org/publicdomain/zero/1.0/legalcode           *
+ **************************************************************************************/
+#include <catch2/catch.hpp>
+
+/* ================================================================================== */
+
+#include <top_level.hpp>
+TEST_CASE( "top_level", "[cpp-nesting]" ) {
+    top_level tl("hiya");
+    REQUIRE( tl.mName == "hiya" );
+}
+
+/* ================================================================================== */
+
+#include <nested/one/one.hpp>
+TEST_CASE( "nested/one/one.hpp", "[cpp-nesting]" ) {
+    nested::one one;
+    REQUIRE( one.compiles() );
+
+    nested::one::params one_params;
+    REQUIRE( one_params.compiles() );
+
+    REQUIRE( sizeof(nested::one::params::four_bytes) == 4 );
+}
+
+/* ================================================================================== */
+
+#include <nested/two/two.hpp>
+TEST_CASE( "nested/two/two.hpp", "[cpp-nesting]" ) {
+    nested::two two;
+    REQUIRE( two.compiles() );
+
+    nested::two::params two_params;
+    REQUIRE( two_params.compiles() );
+
+    REQUIRE( sizeof(nested::four_bytes) == 4 );
+    REQUIRE( sizeof(nested::two::params::four_bytes) == 4 );
+}
+
+/* ================================================================================== */
+
+#include <nested/dual_nested/one/one.hpp>
+TEST_CASE( "nested/dual_nested/one/one.hpp", "[cpp-nesting]" ) {
+    nested::dual_nested::one one;
+    REQUIRE( one.compiles() );
+
+    nested::dual_nested::one::params one_params;
+    REQUIRE( one_params.compiles() );
+
+    REQUIRE( sizeof(nested::dual_nested::one::params::four_bytes) == 4 );
+}
+
+/* ================================================================================== */
+
+#include <nested/dual_nested/two/two.hpp>
+TEST_CASE( "nested/dual_nested/two/two.hpp", "[cpp-nesting]" ) {
+    nested::dual_nested::two two;
+    REQUIRE( two.compiles() );
+
+    nested::dual_nested::two::params two_params;
+    REQUIRE( two_params.compiles() );
+
+    REQUIRE( sizeof(nested::dual_nested::two::params::four_bytes) == 4 );
+}
