ab69386af9b36da3cbb90ad3f749612dc1bf2e0b
sven <sjm324@cornell.edu>
2018-08-13 23:55:48 -0700

    add c_maths c tests, LANGUAGES set in all subdirectories
    - add implementation of c_maths library and tests
        - minor restructure for more classic C library intended to
          be able to be called from C++ :)
    - make sure project(... LANGUAGES ...) are set explicitly for every
      project (added: cpp_long_names and cpp_nesting)

---
+++ testing/projects/c_maths/src/c_maths.c
@@ -0,0 +1,17 @@
+/***************************************************************************************
+ * This file is dedicated to the public domain.  If your jurisdiction requires a       *
+ * specific license:                                                                   *
+ *                                                                                     *
+ * Copyright (c) Stephen McDowell, 2017-2018                                           *
+ * License:      CC0 1.0 Universal                                                     *
+ * License Text: https://creativecommons.org/publicdomain/zero/1.0/legalcode           *
+ **************************************************************************************/
+#include <c_maths.h>
+
+int cm_add(int a, int b) {
+    return a + b;
+}
+
+int cm_sub(int a, int b) {
+    return a - b;
+}
---
+++ testing/projects/c_maths/src/tests.cpp
@@ -0,0 +1,74 @@
+/***************************************************************************************
+ * This file is dedicated to the public domain.  If your jurisdiction requires a       *
+ * specific license:                                                                   *
+ *                                                                                     *
+ * Copyright (c) Stephen McDowell, 2017-2018                                           *
+ * License:      CC0 1.0 Universal                                                     *
+ * License Text: https://creativecommons.org/publicdomain/zero/1.0/legalcode           *
+ **************************************************************************************/
+#include <catch2/catch.hpp>
+#include <c_maths.h>
+
+/* ================================================================================== */
+
+TEST_CASE( "cm_add", "[c-maths]" ) {
+    SECTION( "Zero" ) {
+        REQUIRE( cm_add(0, 0) == 0 );
+    }
+
+    SECTION( "Positive plus positive." ) {
+        REQUIRE( cm_add(0, 1) == 1 );
+        REQUIRE( cm_add(1, 0) == 1 );
+        REQUIRE( cm_add(1, 2) == 3 );
+        REQUIRE( cm_add(2, 1) == 3 );
+    }
+
+    SECTION( "Negative plus negative." ) {
+        REQUIRE( cm_add( 0, -1) == -1 );
+        REQUIRE( cm_add(-1,  0) == -1 );
+        REQUIRE( cm_add(-1, -2) == -3 );
+        REQUIRE( cm_add(-2, -1) == -3 );
+    }
+
+    SECTION( "Negative plus positive." ) {
+        REQUIRE( cm_add(-1, 2) ==  1 );
+        REQUIRE( cm_add(-2, 1) == -1 );
+    }
+
+    SECTION( "Positive plus negative." ) {
+        REQUIRE( cm_add(1, -2) == -1 );
+        REQUIRE( cm_add(2, -1) ==  1 );
+    }
+}
+
+/* ================================================================================== */
+
+TEST_CASE( "cm_sub", "[c-maths]" ) {
+    SECTION( "Zero" ) {
+        REQUIRE( cm_sub(0, 0) == 0 );
+    }
+
+    SECTION( "Positive minus positive." ) {
+        REQUIRE( cm_sub(0, 1) == -1 );
+        REQUIRE( cm_sub(1, 0) ==  1 );
+        REQUIRE( cm_sub(1, 2) == -1 );
+        REQUIRE( cm_sub(2, 1) ==  1 );
+    }
+
+    SECTION( "Negative minus negative." ) {
+        REQUIRE( cm_sub( 0, -1) ==  1 );
+        REQUIRE( cm_sub(-1,  0) == -1 );
+        REQUIRE( cm_sub(-1, -2) ==  1 );
+        REQUIRE( cm_sub(-2, -1) == -1 );
+    }
+
+    SECTION( "Negative minus positive." ) {
+        REQUIRE( cm_sub(-1, 2) == -3 );
+        REQUIRE( cm_sub(-2, 1) == -3 );
+    }
+
+    SECTION( "Positive minus negative." ) {
+        REQUIRE( cm_sub(1, -2) == 3 );
+        REQUIRE( cm_sub(2, -1) == 3 );
+    }
+}
