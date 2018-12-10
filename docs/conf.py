33a91dd5205e8ecd2c59a0f5696c23327ba1bbe7
sven <sjm324@cornell.edu>
2018-12-10 01:54:59 -0800

    ignore doxygen urls with linkcheck
    cuz i'm seriously over this

--- docs/conf.py
+++ docs/conf.py
@@ -50,9 +50,11 @@ intersphinx_mapping = {
     'bs4':    ('https://www.crummy.com/software/BeautifulSoup/bs4/doc', "_intersphinx/bs4_objects.inv")
 }
 
-# make linkcheck does not support GitHub README.md anchors (they are synthetic anchors)
 linkcheck_ignore = [
-    r'https://github.com/jonmiles/bootstrap-treeview#.*'
+    # linkcheck does not support GitHub README.md anchors (they are synthetic anchors)
+    r'https://github\.com/jonmiles/bootstrap-treeview#.*',
+    # frequent outages on doxygen website, annoying enough that I don't care anymore
+    r'.*www\.stack\.nl/~dimitri/doxygen.*'
 ]
 
 # show :autoclass: member definitions as defined in exhale.py
