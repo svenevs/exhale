1dc03ec2959746a079847b103cbfa86b71301197
Stephen McDowell <svenevs.dev@gmail.com>
2021-11-28 21:11:03 -0500

    update license year

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # noqa: D100
 ########################################################################################
-# This file is part of exhale.  Copyright (c) 2017-2019, Stephen McDowell.             #
+# This file is part of exhale.  Copyright (c) 2017-2021, Stephen McDowell.             #
 # Full BSD 3-Clause license available here:                                            #
 #                                                                                      #
 #                https://github.com/svenevs/exhale/blob/master/LICENSE                 #
