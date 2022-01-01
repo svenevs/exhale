bbe9e688653d91dd09bd310367fd05e1a0566d19
Stephen McDowell <svenevs.dev@gmail.com>
2021-12-31 21:02:19 -0500

    update license year

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import exhale
 needs_sphinx = "4.3.1"
 
 project = "Exhale"
-copyright = "2017-2021, Stephen McDowell"
+copyright = "2017-2022, Stephen McDowell"
 author = "Stephen McDowell"
 version = exhale.__version__
 release = exhale.__version__
