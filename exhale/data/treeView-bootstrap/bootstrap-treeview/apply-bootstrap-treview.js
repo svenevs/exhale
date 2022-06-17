4600e4f2c48d2c4365315f085d70929b00bbd23a
Stephen McDowell <svenevs.dev@gmail.com>
2022-06-17 06:04:11 -0400

    Fix sphinx-bootstrap-theme styling
    The introduction of the page hierarchies broke the bootstrap tree for
    any project that does not use `page`.  Solution is to only select /
    apply the `treeview` functions if the id anchors are found.

--- exhale/data/treeView-bootstrap/bootstrap-treeview/apply-bootstrap-treview.js
+++ exhale/data/treeView-bootstrap/bootstrap-treeview/apply-bootstrap-treview.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-    // apply the class view hierarchy
-    $("#class-treeView").treeview({
-        data: getClassViewTree(),
-        enableLinks: true
-    });
-
-    // apply the directory view hierarchy
-    $("#directory-treeView").treeview({
-        data: getDirectoryViewTree(),
-        enableLinks: true
-    });
+    // apply the page view hierarchy if it exists
+    var page_view = $("#page-treeView");
+    if (page_view.length) {
+        page_view.treeview({
+            data: getPageHierarchyTree(),
+            enableLinks: true
+        });
+    }
+    // apply the class view hierarchy if it exists
+    var class_view = $("#class-treeView");
+    if (class_view.length) {
+        class_view.treeview({
+            data: getClassViewTree(),
+            enableLinks: true
+        });
+    }
+    // apply the directory view hierarchy if it exists
+    var dir_view = $("#directory-treeView");
+    if (dir_view.length) {
+        dir_view.treeview({
+            data: getDirectoryViewTree(),
+            enableLinks: true
+        });
+    }
 });
