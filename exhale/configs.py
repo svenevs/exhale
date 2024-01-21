ec9f12cbb1983c80ebc7bacee3e7eaf612ab2cc3
Stephen McDowell <svenevs@users.noreply.github.com>
2024-01-20 22:57:32 -0500

    Update license year to 2024 (#211)

--- exhale/configs.py
+++ exhale/configs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 ########################################################################################
-# This file is part of exhale.  Copyright (c) 2017-2023, Stephen McDowell.             #
+# This file is part of exhale.  Copyright (c) 2017-2024, Stephen McDowell.             #
 # Full BSD 3-Clause license available here:                                            #
 #                                                                                      #
 #                https://github.com/svenevs/exhale/blob/master/LICENSE                 #
